dde953b36309c5e1eb0598520a11ee3d15227547
Rafiqul Hasan <rony.icpc@gmail.com>
2025-11-25 23:10:56 +0100

    added toolbar UI

---
+++ core/ui/src/main/java/com/example/weatherapp/core/ui/AppToolbar.kt
@@ -0,0 +1,144 @@
+package com.example.weatherapp.core.ui
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherapp.core.designsystem.theme.AppTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppStandardToolbar(
+    title: String,
+    onBackClick: (() -> Unit)? = null, // Nullable: if null, don't show back arrow
+    actions: @Composable RowScope.() -> Unit = {},
+    scrollBehavior: TopAppBarScrollBehavior? = null
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(text = title, style = MaterialTheme.typography.titleMedium) },
+        navigationIcon = {
+            if (onBackClick != null) {
+                IconButton(onClick = onBackClick) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        },
+        actions = actions,
+        scrollBehavior = scrollBehavior,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun AppStandardToolbarPreview() {
+    AppTheme {
+        AppStandardToolbar("Weather Info")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun AppStandardToolbarWithNavigationBackPreview() {
+    AppTheme {
+        AppStandardToolbar(
+            title = "Weather Info",
+            onBackClick = {
+
+            }
+        )
+    }
+}
+
+// Specialized Toolbar for Search
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppSearchToolbar(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    onBackClick: () -> Unit,
+    onClearClick: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            TextField(
+                value = query,
+                onValueChange = onQueryChange,
+                placeholder = { Text("Search...") },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+            }
+        },
+        actions = {
+            if (query.isNotEmpty()) {
+                IconButton(onClick = onClearClick) {
+                    Icon(Icons.Filled.Clear, "Clear")
+                }
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun AppSearchToolbarPreview() {
+    AppTheme {
+        AppSearchToolbar(
+            query = "",
+            onQueryChange = {},
+            onBackClick = {},
+            onClearClick = {}
+
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun AppSearchToolbarWithQueryPreview() {
+    AppTheme {
+        AppSearchToolbar(
+            query = "Berlin",
+            onQueryChange = {},
+            onBackClick = {},
+            onClearClick = {}
+
+        )
+    }
+}

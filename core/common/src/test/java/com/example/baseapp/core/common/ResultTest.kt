e2abbbc314fd990b805ab744b339d832e8479113
Rafiqul Hasan <rony.icpc@gmail.com>
2026-02-10 16:51:47 +0100

    added Result class and test

---
+++ core/common/src/test/java/com/example/baseapp/core/common/ResultTest.kt
@@ -0,0 +1,48 @@
+package com.example.baseapp.core.common
+
+import app.cash.turbine.test
+import com.example.baseapp.core.common.result.Result
+import com.example.baseapp.core.common.result.asResult
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class ResultTest {
+    @Test
+    fun `GIVEN value WHEN SUCESS THEN verify loading and success`() = runTest {
+        flow {
+            emit(10)
+        }.asResult()
+            .test {
+                assertEquals(Result.Loading, awaitItem())
+                assertEquals(Result.Success(10), awaitItem())
+                awaitComplete()
+            }
+    }
+
+    @Test
+    fun `WHEN Error THEN verify loading and error`() = runTest {
+        flow<Int> {
+            throw Exception(ERROR_MSG)
+        }.asResult()
+            .test {
+                assertEquals(Result.Loading, awaitItem())
+                when(val result = awaitItem()) {
+                    is Result.Error -> assertEquals(ERROR_MSG, result.exception.message)
+                    Result.Loading,
+                    is Result.Success,
+                        -> throw IllegalStateException(
+                        "The flow should have emitted an Error Result",
+                    )
+                }
+                awaitComplete()
+            }
+    }
+
+    companion object{
+        private const val ERROR_MSG = "Test Exception"
+    }
+}

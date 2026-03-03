cdfc7be6057c9013f69d0fce1a8cd463bb380fbe
Rafiqul Hasan <rony.icpc@gmail.com>
2026-03-03 23:16:46 +0100

    settings public api module added with nav key

---
+++ feature/settings/publicApi/src/main/java/com/example/weatherApp/feature/settings/publicApi/navigation/SettingsNavKey.kt
@@ -0,0 +1,7 @@
+package com.example.weatherApp.feature.settings.publicApi.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+object SettingsNavKey : NavKey
